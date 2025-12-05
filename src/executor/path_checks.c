42b2a6cd3ff82e6620b67949812c273f7c42944a
mikayelyolchyan <mikayelyolchyan@gmail.com>
2025-12-05 23:31:26 +0400

    feat: Add directory check in file access validation

--- src/executor/path_checks.c
+++ src/executor/path_checks.c
@@ -11,6 +11,21 @@
 /* ************************************************************************** */
 
 #include "../../include/executor/executor.h"
+#include <sys/stat.h>
+
+static void	check_is_directory(char *cmd_path)
+{
+	struct stat	path_stat;
+
+	if (stat(cmd_path, &path_stat) == 0 && S_ISDIR(path_stat.st_mode))
+	{
+		write(2, "minishell: ", 11);
+		write(2, cmd_path, ft_strlen(cmd_path));
+		write(2, ": Is a directory\n", 17);
+		free(cmd_path);
+		exit(126);
+	}
+}
 
 void	check_file_access(char *cmd_path)
 {
@@ -22,6 +37,7 @@ void	check_file_access(char *cmd_path)
 		free(cmd_path);
 		exit(127);
 	}
+	check_is_directory(cmd_path);
 	if (access(cmd_path, X_OK) != 0)
 	{
 		write(2, "minishell: ", 11);
